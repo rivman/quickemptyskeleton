68127ec8ea3078d83b0ae32e565bcc864ece14b1
RivDesign <rubeluta@gmail.com>
2022-01-16 23:11:31 +0100

    Update global.inc.php

--- pml/inc/global.inc.php
+++ pml/inc/global.inc.php
@@ -150,38 +150,6 @@ define( 'TIME_ZONE_SUPPORT_URL' , 'http://support.pimpmylog.com/discussions/prob
 
 /*
 |--------------------------------------------------------------------------
-| Disable magic quotes
-|--------------------------------------------------------------------------
-|
-| PHP 5.2 and 5.3 can have magic quotes enabled on the whole PHP install.
-| http://support.pimpmylog.com/discussions/problems/56-regex-tester-match-is-not-a-valid-associative-array
-|
-*/
-if ( get_magic_quotes_gpc() )
-{
-	$process = array( &$_GET , &$_POST , &$_COOKIE , &$_REQUEST );
-	while ( list( $key , $val ) = each( $process ) )
-	{
-		foreach ( $val as $k => $v )
-		{
-			unset( $process[ $key ][ $k ] );
-			if ( is_array( $v ) )
-			{
-				$process[ $key ][ stripslashes( $k ) ] = $v;
-				$process[]                             = &$process[ $key ][ stripslashes( $k ) ];
-			}
-			else
-			{
-				$process[ $key ][ stripslashes( $k ) ] = stripslashes( $v );
-			}
-		}
-	}
-	unset( $process );
-}
-
-
-/*
-|--------------------------------------------------------------------------
 | Global internal parameters
 |--------------------------------------------------------------------------
 |
